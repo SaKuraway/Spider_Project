3cdb16777288178511d59cf99459dd4775d949b0
sakurapan <sakurapan@0b7ee8a0-cf4d-11e7-9356-f1b2cbd46caf>
2017-12-08 09:21:22 +0000

    运行版

--- TianTianJiJin/TianTianJiJin/connect_to_mysql.py
+++ TianTianJiJin/TianTianJiJin/connect_to_mysql.py
@@ -8,10 +8,15 @@ def main():
     try:
         # 使用cursor()方法获取操作游标
         cur = mysql_client.cursor()
-        code = '002196'
-        date = "2017-06-30"
-        count = cur.execute("select * from 财务报表 where Fund_code="+"'"+code+"'"+" and Financial_index_date="+"'"+date +"'"+";")
-        print(count)
+        # code = '002196'
+        # date = "2017-06-30"
+        # count = cur.execute("select * from 财务报表 where Fund_code="+"'"+code+"'"+" and Financial_index_date="+"'"+date +"'"+";")
+        count1 = cur.execute("ALTER TABLE 科学研究和技术服务业行业市盈率 DROP id;")
+        count2 = cur.execute("ALTER  TABLE 科学研究和技术服务业行业市盈率 ADD id MEDIUMINT( 8 ) NOT NULL  FIRST;")
+        count3 = cur.execute("ALTER  TABLE 科学研究和技术服务业行业市盈率 MODIFY COLUMN id MEDIUMINT( 8 ) NOT NULL  AUTO_INCREMENT,ADD PRIMARY  KEY(id);")
+        print(count1)
+        print(count2)
+        print(count3)
         # print(cur.fetchall())
         # cur.execute("insert into 交通运输、仓储和邮政业行业市盈率(id,date,static_PE) VALUES(%s,%s,%s)",
         #             (int(flag), int((int(item[0]) / 1000)), float(item[1])))
